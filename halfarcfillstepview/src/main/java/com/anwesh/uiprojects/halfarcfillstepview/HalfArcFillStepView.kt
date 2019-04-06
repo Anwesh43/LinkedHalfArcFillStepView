7a9772ad2adaf3b4626fedc462e024a7718e3deb
Anwesh43 <anweshthecool0@gmail.com>
2019-04-06 10:36:29 +0530

    adding Int and Float extension methods

--- halfarcfillstepview/src/main/java/com/anwesh/uiprojects/halfarcfillstepview/HalfArcFillStepView.kt
+++ halfarcfillstepview/src/main/java/com/anwesh/uiprojects/halfarcfillstepview/HalfArcFillStepView.kt
@@ -18,3 +18,14 @@ val strokeFactor : Int = 90
 val sizeFactor : Float = 2.9f
 val foreColor : Int = Color.parseColor("#9C27B0")
 val backColor : Int = Color.parseColor("#BDBDBD")
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
+fun Float.mirror(a : Float, b : Float) : Float = (1 - this) * a + this * b
+fun Float.mirrorValue(a : Int, b : Int) : Float {
+    val k : Float = scaleFactor()
+    return k.mirror(a.inverse(), b.inverse())
+}
+fun Float.updateValue(dir : Int, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
