8545bb32da3adffddc557ed82af787aa0c98ca1d
Anwesh43 <anweshthecool0@gmail.com>
2019-04-06 10:49:55 +0530

    adding drawHAFNode canvas extension method

--- halfarcfillstepview/src/main/java/com/anwesh/uiprojects/halfarcfillstepview/HalfArcFillStepView.kt
+++ halfarcfillstepview/src/main/java/com/anwesh/uiprojects/halfarcfillstepview/HalfArcFillStepView.kt
@@ -29,3 +29,58 @@ fun Float.mirrorValue(a : Int, b : Int) : Float {
     return k.mirror(a.inverse(), b.inverse())
 }
 fun Float.updateValue(dir : Int, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
+
+fun Canvas.drawXY(x : Float, y : Float, cb : (Canvas) -> Unit) {
+    save()
+    translate(x, y)
+    cb(this)
+    restore()
+}
+
+fun Canvas.drawRotateDeg(deg : Float, cb : (Canvas) -> Unit) {
+    save()
+    rotate(deg)
+    cb(this)
+    restore()
+}
+
+fun Canvas.drawClip(path : Path, cb : (Canvas) -> Unit) {
+    save()
+    clipPath(path)
+    cb(this)
+    restore()
+}
+
+fun Canvas.drawHalfArc(x : Float, sc1 : Float, sc2 : Float, r : Float, paint : Paint) {
+    drawXY(x, 0f) {
+        it.drawRotateDeg(180f * sc1) {
+            val arcRect : RectF = RectF(-r, -r, r, r)
+            val arcDeg : Float = 180f
+            val path : Path = Path()
+            path.addArc(arcRect, arcDeg, arcDeg)
+            it.drawClip(path) {
+                paint.style = Paint.Style.FILL
+                drawRect(RectF(-r, -r, r, -r + r * sc2), paint)
+            }
+            paint.style = Paint.Style.STROKE
+            drawArc(RectF(arcRect), arcDeg, arcDeg, false, paint)
+        }
+    }
+}
+
+fun Canvas.drawHAFNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    val xGap : Float = size / (arcs)
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    drawXY(w / 2, gap * (i + 1)) {
+        for (j in 0..(arcs)) {
+            val scj1 : Float = sc1.divideScale(j, arcs)
+            val scj2 : Float = sc2.divideScale(j, arcs)
+            it.drawHalfArc(-size + xGap * j, scj1, scj2, xGap / 2, paint)
+        }
+    }
+}
