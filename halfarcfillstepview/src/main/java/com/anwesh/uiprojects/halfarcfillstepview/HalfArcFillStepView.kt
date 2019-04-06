cccb14bc90896672fedd58bf9016b018c7617c06
Anwesh43 <anweshthecool0@gmail.com>
2019-04-06 10:32:28 +0530

    adding libraries and constants

--- halfarcfillstepview/src/main/java/com/anwesh/uiprojects/halfarcfillstepview/HalfArcFillStepView.kt
+++ halfarcfillstepview/src/main/java/com/anwesh/uiprojects/halfarcfillstepview/HalfArcFillStepView.kt
@@ -3,3 +3,18 @@ package com.anwesh.uiprojects.halfarcfillstepview
 /**
  * Created by anweshmishra on 06/04/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.*
+
+val nodes : Int = 5
+val arcs : Int = 2
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#9C27B0")
+val backColor : Int = Color.parseColor("#BDBDBD")
