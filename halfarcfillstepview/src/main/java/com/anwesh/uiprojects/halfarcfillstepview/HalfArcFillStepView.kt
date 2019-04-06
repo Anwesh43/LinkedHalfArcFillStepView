ddcfdf764b3acfeb08ef9ca1fc3d7079248b7266
Anwesh43 <anweshthecool0@gmail.com>
2019-04-06 11:04:15 +0530

    adding HalfArcFillStep class

--- halfarcfillstepview/src/main/java/com/anwesh/uiprojects/halfarcfillstepview/HalfArcFillStepView.kt
+++ halfarcfillstepview/src/main/java/com/anwesh/uiprojects/halfarcfillstepview/HalfArcFillStepView.kt
@@ -193,4 +193,28 @@ class HalfArcFillStepView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class HalfArcFillStep(var i : Int) {
+
+        private val root : HAFNode = HAFNode(0)
+        private var curr : HAFNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
